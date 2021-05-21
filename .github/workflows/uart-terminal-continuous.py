b33f35bfb84a9eefab5cb0aea99cf86c727da427
Lucas Zacchi <zacchilucasm@gmail.com>
2021-05-21 12:13:54 -0300

    Fix string formatting

--- .github/workflows/uart-terminal-continuous.py
+++ .github/workflows/uart-terminal-continuous.py
@@ -12,11 +12,11 @@ if 2 < len(sys.argv) < 6:
     device = sys.argv[1]
     baudrate = sys.argv[2]
     timeout = int(sys.argv[3])
-    if len(sys.argv) == 5:
-        logfile = sys.argv[4]
-        logflag = 1
-    else:
-        logflag = 0
+    # if len(sys.argv) == 5:
+    #     logfile = sys.argv[4]
+    #     logflag = 1
+    # else:
+    #     logflag = 0
 else:
     print('\n')
     print('Wrong arguments')
@@ -27,9 +27,9 @@ else:
     print('\n')
     sys.exit(1)
 
-if logflag:
-    logging.basicConfig(filename=logfile, encoding='utf-8',
-                        level=logging.INFO)
+# if logflag:
+#     logging.basicConfig(filename=logfile, encoding='utf-8',
+#                         level=logging.INFO)
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
@@ -67,11 +67,11 @@ with serial.Serial(device, baudrate, timeout=0.5) as ser:
     # that way we can control for how long we'll log the execution
     log_process = Process(target=read, name='Process_inc_forever')
     log_process.start()
-    log_process.join(timeout=timeout)
+    log_process.join(timeout=int(timeout))
     log_process.terminate()
 
     if log_process.exitcode is None:
-        logging.info(f'process {log_process} terminated after {timeout} seconds')
+        logging.info('process terminated after %s seconds', timeout)
         logging.info('Closing serial port')
         sleep(1)
         ser.close()
