2f4997650724e843bb5dd1164ce42a9ed794b893
Lucas Zacchi <zacchilucasm@gmail.com>
2021-06-02 12:36:05 -0300

    Add logging handlers to print to console and save to file

--- .github/workflows/uart-terminal-continuous.py
+++ .github/workflows/uart-terminal-continuous.py
@@ -12,11 +12,11 @@ if 2 < len(sys.argv) < 6:
     device = sys.argv[1]
     baudrate = sys.argv[2]
     timeout = int(sys.argv[3])
-    # if len(sys.argv) == 5:
-    #     logfile = sys.argv[4]
-    #     logflag = 1
-    # else:
-    #     logflag = 0
+    if len(sys.argv) == 5:
+        logfile = sys.argv[4]
+        logflag = 1
+    else:
+        logflag = 0
 else:
     print('\n')
     print('Wrong arguments')
@@ -27,11 +27,15 @@ else:
     print('\n')
     sys.exit(1)
 
-# if logflag:
-#     logging.basicConfig(filename=logfile, encoding='utf-8',
-#                         level=logging.INFO)
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+stdout_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stdout_handler)
+
+if logflag:
+    output_handler = logging.FileHandler(logfile)
+    logger.addHandler(output_handler)
 
 
 # Creating read process fuction
@@ -41,11 +45,11 @@ def read():
         if data != b'' and data[0] == 0x0d:
             # Remove the Carriage Return character
             output = str(datetime.now()) + ">    " + data[1:].decode('utf-8')
-            logging.info(output)
+            logger.info(output)
 
         elif data != b'':
             output = str(datetime.now()) + ">    " + data.decode('utf-8')
-            logging.info(output)
+            logger.info(output)
 
 
 # Define serial port
@@ -65,14 +69,14 @@ with serial.Serial(device, baudrate, timeout=0.5) as ser:
     # This defines a process which will execute the 'read' function
     # after a predetermined ammount of time, it will terminate
     # that way we can control for how long we'll log the execution
-    log_process = Process(target=read, name='Process_inc_forever')
+    log_process = Process(target=read, name='Logging_Process')
     log_process.start()
     log_process.join(timeout=int(timeout))
     log_process.terminate()
 
     if log_process.exitcode is None:
-        logging.info('process terminated after %s seconds', timeout)
-        logging.info('Closing serial port')
+        logger.info('process terminated after %s seconds', timeout)
+        logger.info('Closing serial port')
         sleep(1)
         ser.close()
         sys.exit(0)
